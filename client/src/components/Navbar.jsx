babafaa417c753fbc42c2fc332666a2fb145a12a
Alan Sanchez <alnwolff@gmail.com>
2023-08-05 21:22:07 +0200

    fix navbar appearance and fronten_url variable

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -29,14 +29,14 @@ function Navbar() {
       </a>
 
       <h1 className="py-4 mb-5 text-xl font-bold text-[#636566] text-center flex justify-center gap-5 tracking-widest">
-        OUTUBRO 21, 2023{" "}
+        OUTUBRO 21, 2023
         <i className="fa-solid fa-circle text-xs self-center "></i> SALÃO
         ESPORTE FINO
       </h1>
       <div className="text-[#636566] tracking-widest m-3">
         <CountdownTimer targetDate={weedingDay} />
       </div>
-      <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+      <div className="w-full md:block md:w-auto" id="navbar-default">
         <div className="gap-4 tracking-wider font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ">
           <Link
             className="text-base text-[#636566] hover:underline focus:underline decoration-[#9ba5aa] "
