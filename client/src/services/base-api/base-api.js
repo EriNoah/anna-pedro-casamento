15bf2d610b6d262095a6b077fe5c203f47bb8307
Alan Sanchez <alnwolff@gmail.com>
2023-08-05 02:34:29 +0200

    fix the api URL

--- client/src/services/base-api/base-api.js
+++ client/src/services/base-api/base-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: process.env.REACT_APP_BASE_API_URL || "http://localhost:5005/api/v1",
+  baseURL: "https://anna-pedro-d505b9d011fd.herokuapp.com/api/v1",
 });
 
 export default http;
