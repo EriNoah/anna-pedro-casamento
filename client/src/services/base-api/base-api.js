41ff93375803f23c32fe7c7bfdaadafed5d910d7
Alan Sanchez <alnwolff@gmail.com>
2023-08-08 00:40:17 +0200

    fix route to the api and translation issue

--- client/src/services/base-api/base-api.js
+++ client/src/services/base-api/base-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "https://anna-pedro-d505b9d011fd.herokuapp.com/api/v1",
+  baseURL: "https://www.annaepedro.site/api/v1",
 });
 
 export default http;
