9463782ed13d5ff0ae674a5de6b54b9f4122d81a
Alan Sanchez <alnwolff@gmail.com>
2023-08-08 00:53:56 +0200

    fix frontend url

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 // unless the request if from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL = "https://anna-pedro-d505b9d011fd.herokuapp.com";
+const FRONTEND_URL = "https://www.annaepedro.site";
 
 // Middleware configuration
 module.exports = (app) => {
