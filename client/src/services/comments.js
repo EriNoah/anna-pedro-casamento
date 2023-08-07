cf813820ab34e5e6f2b781d343e01a9d15250f77
Alan Sanchez <alnwolff@gmail.com>
2023-08-08 01:40:01 +0200

    fix api call for messages

--- client/src/services/comments.js
+++ client/src/services/comments.js
@@ -1,7 +1,13 @@
 import http from "./base-api/base-api";
 
-const create = (comment) => http.post("/comments", comment);
-const list = () => http.get("/comments");
+const create = (comment) =>
+  http.post("/comments", comment, {
+    headers: { "Access-Control-Allow-Origin": "*" },
+  });
+const list = () =>
+  http.get("/comments", {
+    headers: { "Access-Control-Allow-Origin": "*" },
+  });
 
 const commentService = {
   create,
