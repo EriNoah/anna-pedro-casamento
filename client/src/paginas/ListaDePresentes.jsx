e4efabb5bd768ae93748934c6891bd55e325b7ec
Alan Sanchez <alnwolff@gmail.com>
2023-08-17 19:23:08 +0200

    add apoiase link

--- client/src/paginas/ListaDePresentes.jsx
+++ client/src/paginas/ListaDePresentes.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import houseConstructionService from '../services/houseConstruction';
 import {getHouseImage} from "../utils/houseUtils"
+import apoiase from "../assets/apoiase.png"
+
 
 function ListaDePresentes() {
   const [houseState, setHouseState] = useState({});
@@ -19,25 +21,28 @@ function ListaDePresentes() {
   
   }, [houseState.length]);
 
-  const handleClick = () => {
-    try {
-      houseConstructionService.addPiece({
-        soldPieces: 10,
-        remainingPieces: 100,
-        totalPieces: 100,
-      });
-    } catch(error) {
-      console.error(error);
-    }
-  }
-
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
+        <h1 className="py-5 text-2xl font-bold border-[#636566] text-[#636566] text-center">
+        Ajude-nos a construir nossa casa!
+      </h1>
+      <div className="text-sm mb-3 md:text-base">
+        Oi, pessoal,
+        Como sabem, nós moramos juntos há um tempinho, mas sempre tivemos o desejo de festejar com vocês o nosso encontro e nossa alegria em partilhar a vida.
+        Nesse percurso, alguns de vocês participaram intensamente dessa construção e realização do desejo de morarmos juntos. Tudo começou com nossa ida para São João del Rei durante a pandemia, e permanecemos nesse propósito firmes e fortes. Foi um belo estágio para reafirmamos nosso amor e vontade de seguirmos juntos.
+        Hoje, temos tudo que precisamos para nossa casinha. E como só falta ela, a maravilhosa casinha, rsrs... Pensamos em deixar como sugestão de presentes que contribuam para que realizemos esse lindo sonho.
+        Somos muito gratos pelo amor e carinho de vocês, e esperamos poder nos divertir muito nesse dia tão especial.
+        Amamos vocês, 
+        Anna e Pedro
+      </div>
       {houseState.soldPieces > 0 && (
-        <img src={getHouseImage(houseState.soldPieces)} alt={"casa"} />
+        <img className='w-5/12' src={getHouseImage(houseState.soldPieces)} alt={"casa"} />
       )}
-        <button onClick={handleClick}>Submit</button>
-        <h1>Ajude-nos a construir nossa casa!</h1>
+      <div>
+        Wanna colaborate with our dream?
+        Please click in the image to be redirect to our apoia.se's page! 
+      </div>
+      <a href="https://apoia.se/annaepedro" target="_blank" rel="noreferrer"><img src={apoiase} className="w-4/12" alt='apoiase img'/></a>
     </div>
   );
 }
