cbd8c3640b8733766a167f91236a521983a23a14
Erica Sanchez <ericazach@gmail.com>
2023-08-21 20:12:48 +0200

    Add: puzzle image

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="flex flex-col">
       <div className="flex m-1 gap-3">
-        <div className="uppercase text-base mb-3 text-[#636566] tracking-widest border-l-2">
+        <div className="uppercase text-sm md:text-base mb-3 text-[#636566] tracking-widest border-l-2">
           <i className="fa-solid fa-at me-1"></i>2023 Erica & Alan
         </div>
         <div className="text-[#636566] tracking-wide ">All rights reserved</div>
