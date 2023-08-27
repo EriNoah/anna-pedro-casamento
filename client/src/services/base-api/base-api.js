5ca1c79bae30f0f820fac880548b42d7ffbfb62a
Erica Sanchez <ericazach@gmail.com>
2023-08-27 21:05:35 +0200

    Fix: services route

--- client/src/services/base-api/base-api.js
+++ client/src/services/base-api/base-api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  // baseURL: "https://www.annaepedro.site/api/v1",
-  baseURL: "http://localhost:5005/api/v1",
+  baseURL: "https://www.annaepedro.site/api/v1",
 });
 
 export default http;
