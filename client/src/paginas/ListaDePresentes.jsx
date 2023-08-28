96fa6ea1362ddbcfc39a8587d27710b3c6226747
Alan Sanchez <alnwolff@gmail.com>
2023-08-28 19:22:13 +0200

    refactor ListaDePresentes to make it simple and readable

--- client/src/paginas/ListaDePresentes.jsx
+++ client/src/paginas/ListaDePresentes.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-import houseConstructionService from "../services/houseConstruction";
-import { getHouseImage } from "../utils/houseUtils";
+import React from "react";
+// import { useEffect } from "react";
+// import houseConstructionService from "../services/houseConstruction";
+// import { getHouseImage } from "../utils/houseUtils";
 import apoiase from "../assets/apoiase.png";
 import "./ListaDePresentes.css";
+import qrcode from "../assets/pix_annaepedro.png";
 
 function ListaDePresentes() {
-  const [houseState, setHouseState] = useState({});
+  // const [houseState, setHouseState] = useState({});
 
-  useEffect(() => {
-    const getData = async () => {
-      await houseConstructionService
-        .getPieces()
-        .then((res) => setHouseState(res.data[0]))
-        .catch((err) => console.log(err));
-    };
-    if (Object.keys(houseState.length === 0)) {
-      getData();
-    }
-  }, [houseState.length]);
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     await houseConstructionService
+  //       .getPieces()
+  //       .then((res) => setHouseState(res.data[0]))
+  //       .catch((err) => console.log(err));
+  //   };
+  //   if (Object.keys(houseState.length === 0)) {
+  //     getData();
+  //   }
+  // }, [houseState.length]);
 
   return (
     <div className="flex flex-col justify-center items-center my-10">
       <h1 className="text-[#636566] tracking-wide text-2xl md:text-[25px]  font mb-7">
         Ajude-nos a construir nossa casa!
       </h1>
-      <div className="text-sm mb-20 md:text-base w-auto md:w-[800px] text-center px-5">
+      <div className="text-sm mb-10 md:text-base w-auto md:w-[800px] text-center px-5">
         Oi, pessoal, <br /> <br />
         Como sabem, nós moramos juntos há um tempinho, mas sempre tivemos o
         desejo de festejar com vocês o nosso encontro e nossa alegria em
@@ -42,9 +43,48 @@ function ListaDePresentes() {
         poder nos divertir muito nesse dia tão especial. Amamos vocês Anna e
         Pedro
       </div>
-      <div
+      <div className="flex justify-center items-start">
+        <i
+          className="fa-solid fa-house-chimney-window mx-5 scale-150 mt-2"
+          style={{ color: "#24703f" }}
+        />
+        <h1 className="text-[#636566] tracking-wide text-2xl md:text-[25px]  font md:mb-7">
+          Quer colaborar com nosso sonho?
+        </h1>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center items-center md:w-[800px]">
+        <div
+              class="p-4 ml-5 mr-5 mt-5 text-sm text-center md:text-lg text-green-800 rounded-lg bg-green-100 dark:bg-gray-800 dark:text-green-400"
+              role="alert"
+            >
+          <p>Escaneie o código QR e envie-nos um PIX (dependendo do seu banco, poderá ser feito um PIX também com cartão de crédito). ❤️ <br/> <br/> Alternativamente, você pode também nos presentear através da plataforma apoia.se clicando na imagem ao fim da página. Pedimos, no entanto, que o envio por PIX seja priorizado, pois a plataforma apoia.se nos cobra uma taxa de 6% em cada doação. <br/><br/> Agradecemos muito a sua contribuição!</p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <div
+              class="rounded-lg bg-green-100 dark:bg-gray-800 dark:text-green-400 p-4 ml-5 mr-5 mt-5 md:ml-1 md:mr-1  text-sm text-center flex flex-col justify-center items-center"
+              role="alert"
+            >
+              <p>PIX</p>
+              <img className="w-7/12 h-7/12" src={qrcode} alt="pix qrcode" />
+            </div>
+            <div
+              class="rounded-lg bg-green-100 dark:bg-gray-800 dark:text-green-400 p-4 ml-5 mr-5 mt-5 md:ml-1 md:mr-1 flex flex-col justify-center items-center"
+              role="alert"
+            >
+            <a
+              className="flex justify-center"
+              href="https://apoia.se/annaepedro"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={apoiase} className="w-10/12" alt="apoiase img" />
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* <div
         div
-        className="w-[340px] h-[306px] md:w-[680px] md:h-[612px] flex justify-start mx-2 background"
+        className="ml-10 mt-20 w-[340px] h-[306px] md:w-[680px] md:h-[612px] flex justify-start mx-2 background"
       >
         <div className="self-end">
           {houseState.soldPieces > 0 && houseState.soldPieces <= 10 && (
@@ -62,27 +102,7 @@ function ListaDePresentes() {
             />
           )}
         </div>
-      </div>
-
-      <div
-        class="p-4 mx-2 my-5 text-sm text-center md:text-lg text-green-800 rounded-lg bg-green-100 dark:bg-gray-800 dark:text-green-400"
-        role="alert"
-      >
-        <i
-          className="fa-solid fa-house-chimney-window mx-2"
-          style={{ color: "#24703f" }}
-        />
-        Quer colaborar com nosso sonho? Clique na imagem para ser redirecionado
-        para a página do apoia.se!
-      </div>
-      <a
-        className="flex justify-center"
-        href="https://apoia.se/annaepedro"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={apoiase} className="w-6/12" alt="apoiase img" />
-      </a>
+      </div> */}
     </div>
   );
 }
